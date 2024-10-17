2b98ae525b12acba33794245d3afd37c8c59bb00
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-10-17 18:55:40 +0800

    Update RPCA_ADMMn.m

--- RPCA/RPCA_ADMMn.m
+++ RPCA/RPCA_ADMMn.m
@@ -1,10 +1,27 @@
 % Our algorithm for solving the RPCA problem
-% Input: the observation matrix M, the L1-norm parameter theta, the F-norm paramter w, the penalty parameter beta
-% Output: the reconstructed sparse matrix S, low-rank matrix L, observation matrix T; relative change chg, number of iterations iter, running time
-% Written by: Zekun Liu (17/12/2023)
+%
+% Input: 
+%         M: the observation matrix 
+%     theta: the L1-norm parameter
+%         w: the F-norm paramter
+%      beta: the penalty parameter
+%
+% Output: 
+%         S: the reconstructed sparse matrix
+%         L: the low-rank matrix
+%         T: the observation matrix
+%       chg: relative change 
+%      iter: number of iterations
+%      time: running time
+%
+% Written by Zekun Liu, 17/12/2023
+%
 % Reference:
-% [1] Z. Liu. An Extended ADMM for 3-Block Nonconvex Nonseparable Problems With Applications. arXiv:2402.02193.
-% Latest Revision: 20/09/2024
+% [1] Z. Liu. 
+%     An Extended ADMM for 3-Block Nonconvex Nonseparable Problems With Applications.
+%     arXiv:2402.02193.
+%
+% Latest Revision: 17/10/2024
 
 
 function [L, S, T, chg, iter, time] = RPCA_ADMMn(M, theta, w, beta)
