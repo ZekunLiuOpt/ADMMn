b89d49497615b8c7eb8bb3a8c97024b2c4a044d4
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-10-17 18:59:24 +0800

    Update RPCA_pADMMz.m

--- RPCA/RPCA_pADMMz.m
+++ RPCA/RPCA_pADMMz.m
@@ -1,11 +1,27 @@
 % Existed algorithm for solving the RPCA problem
-% Input: the observation matrix M, the L1-norm parameter theta, the F-norm paramter w, the penalty parameter beta
-% Output: the reconstructed sparse matrix S, low-rank matrix L, observation matrix T; relative change chg, number of iterations iter, running time
-% Written by: Zekun Liu (18/12/2023)
+%
+% Input: 
+%         M: the observation matrix 
+%     theta: the L1-norm parameter
+%         w: the F-norm paramter
+%      beta: the penalty parameter
+%
+% Output: 
+%         S: the reconstructed sparse matrix
+%         L: the low-rank matrix
+%         T: the observation matrix
+%       chg: relative change 
+%      iter: number of iterations
+%      time: running time
+%
+% Written by Zekun Liu, 18/12/2023
+%
 % Reference:
-% [1] C. Zhang, Y. Song, X. Cai and D. Han. An extended proximal ADMM algorithm for three-block nonconvex optimization problems. 
+% [1] C. Zhang, Y. Song, X. Cai and D. Han. 
+%     An extended proximal ADMM algorithm for three-block nonconvex optimization problems. 
 %     Journal of Computational and Applied Mathematics, 398 (2021), 113681.
-% Latest Revision: 20/09/2024
+%
+% Latest Revision: 17/10/2024
 
 
 function [L, S, T, chg, iter, time] = RPCA_pADMMz(M, theta, w, beta)
