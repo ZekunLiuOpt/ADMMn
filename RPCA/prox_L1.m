2df8f8edfa7402cbbdaf289b6df1de0cc59c425d
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-10-17 18:51:31 +0800

    Update prox_L1.m

--- RPCA/prox_L1.m
+++ RPCA/prox_L1.m
@@ -1,14 +1,23 @@
 % Solf shrinkage operator
-% Input: a given matrix X, with the parameter t
-% Output: the soft-shrink matrix Y
-% Written by: Zekun Liu (16/12/2023)
+%
+% Input: 
+%         X: a given matrix
+%         t: the shrinkage parameter 
+%
+% Output: 
+%         Y: the soft-shrink matrix 
+%
+% Written by Zekun Liu, 16/12/2023
+%
 % Reference:
-% [1] I. Daubechies, M. Defrise and C. D. Mol. An iterative thresholding algorithm for linear inverse problems with a sparsity constraint. 
+% [1] I. Daubechies, M. Defrise and C. D. Mol. 
+%     An iterative thresholding algorithm for linear inverse problems with a sparsity constraint. 
 %     Communications on Pure and Applied Mathematics, 57 (2004), 1413–1457.
-% Latest Revision: 20/09/2024
+%
+% Latest Revision: 17/10/2024
 
 
-function Y = prox_L1(X,t)
+function Y = prox_L1(X, t)
 
 Y = sign(X) .* max(abs(X) - t, 0);
 
