02237d3f83d10e1f855cd168818e82b1b8d301eb
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-10-17 19:19:49 +0800

    Update location.m

--- NMC/location.m
+++ NMC/location.m
@@ -1,8 +1,15 @@
 % Function that generates the matrix with p values 1 and others 0 which belongs to R^{m*n}
-% Input: the number of nonzero entries p, the size of location matrix m and n
-% Output: the location matrix P
-% Written by: Zekun Liu (23/09/2023)
-% Latest Revision: 20/09/2024
+%
+% Input: 
+%         p: the number of nonzero entries
+%      m, n: the size of location matrix
+%
+% Output: 
+%         P: the location matrix 
+%
+% Written by Zekun Liu, 23/09/2023
+%
+% Latest Revision: 17/10/2024
 
 
 function P = location(m, n, p)
