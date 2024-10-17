ff961e57bad19045948c0147d6a3a0dd3887f6d6
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-10-17 19:15:05 +0800

    Update plot_err.m

--- RPCA/plot/plot_err.m
+++ RPCA/plot/plot_err.m
@@ -1,6 +1,8 @@
 % Experiments that comparing the change of the relative error of different algorithms
-% Written by: Zekun Liu (18/12/2023)
-% Latest Revision: 20/09/2024
+%
+% Written by Zekun Liu, 18/12/2023
+%
+% Latest Revision: 17/10/2024
 
 
 clear
@@ -9,7 +11,7 @@ clc
 p = 100;
 n = 100;
 
-% noiseless case, (0.05，10)
+% noiseless case, (0.05, 10)
 spr = 0.05;
 rank = 10;
 
@@ -32,20 +34,21 @@ err11 = RPCA_ADMMn_ERR(M, theta, w, beta1, L, S, T);
 
 err12 = RPCA_pADMMz_ERR(M, theta, w, beta1, L, S, T);
 
-err13 = RPCA_IPPS_ADMM_ERR(M, theta, w, beta1, L, S, T);
+err13 = RPCA_IPPS_ADMM_ERR(M, theta, w, beta2, L, S, T);
 
 err14 = RPCA_PRS_ADMM_ERR(M, theta, w, beta1, L, S, T);
 
 figure;
 ind = 0:3000;
-plot(ind, log10(err14), '-.', 'color', '#77AC30', 'Linewidth', 1.25);
+plot(ind, log10(err14), '-.', 'color', '#77AC30', 'Linewidth', 2);
 hold on;
-plot(ind, log10(err12), '--', 'color', 'm', 'Linewidth', 1.25);
+plot(ind, log10(err12), '--', 'color', '#A066D3', 'Linewidth', 2);
 hold on;
-plot(ind, log10(err13), ':', 'color', '#EDB120', 'Linewidth', 1.75);
+plot(ind, log10(err13), '-.', 'color', '#f97306', 'Linewidth', 2);
 hold on;
-plot(ind, log10(err11), '-', 'color', '#0072BD', 'Linewidth', 1.25);
+plot(ind, log10(err11), '-', 'color', '#0072BD', 'Linewidth', 2);
 hold off;
+grid on; 
 set(gca, 'FontName', 'Times New Roman', 'Fontsize', 11);
 h = legend('PSR-ADMM', 'pADMMz', 'IPPS-ADMM', 'ADMMn', 'Location', 'northeast');
 set(h, 'FontSize', 8);
@@ -53,7 +56,7 @@ xlabel('Iterations');
 ylabel('log_{10}(RelErr)');
 
 
-% noiseless case, (0.05，20)
+% noiseless case, (0.05, 20)
 spr = 0.05;
 rank = 20;
 
@@ -76,20 +79,21 @@ err21 = RPCA_ADMMn_ERR(M, theta, w, beta1, L, S, T);
 
 err22 = RPCA_pADMMz_ERR(M, theta, w, beta1, L, S, T);
 
-err23 = RPCA_IPPS_ADMM_ERR(M, theta, w, beta1, L, S, T);
+err23 = RPCA_IPPS_ADMM_ERR(M, theta, w, beta2, L, S, T);
 
 err24 = RPCA_PRS_ADMM_ERR(M, theta, w, beta1, L, S, T);
 
 figure;
 ind = 0:3000;
-plot(ind, log10(err24), '-.', 'color', '#77AC30', 'Linewidth', 1.25);
+plot(ind, log10(err24), '-.', 'color', '#77AC30', 'Linewidth', 2);
 hold on;
-plot(ind, log10(err22), '--', 'color', 'm', 'Linewidth', 1.25);
+plot(ind, log10(err22), '--', 'color', '#A066D3', 'Linewidth', 2);
 hold on;
-plot(ind, log10(err23), ':', 'color', '#EDB120', 'Linewidth', 1.75);
+plot(ind, log10(err23), '-.', 'color', '#f97306', 'Linewidth', 2);
 hold on;
-plot(ind, log10(err21), '-', 'color', '#0072BD', 'Linewidth', 1.25);
+plot(ind, log10(err21), '-', 'color', '#0072BD', 'Linewidth', 2);
 hold off;
+grid on; 
 set(gca, 'FontName', 'Times New Roman', 'Fontsize', 11);
 h = legend('PSR-ADMM', 'pADMMz', 'IPPS-ADMM', 'ADMMn', 'Location', 'northeast');
 set(h, 'FontSize', 8);
@@ -97,7 +101,7 @@ xlabel('Iterations');
 ylabel('log_{10}(RelErr)');
 
 
-% noisy case, (0.05，10)
+% noisy case, (0.05, 10)
 spr = 0.05;
 rank = 10;
 
@@ -120,21 +124,22 @@ err31 = RPCA_ADMMn_ERR(M, theta, w, beta1, L, S, T);
 
 err32 = RPCA_pADMMz_ERR(M, theta, w, beta1, L, S, T);
 
-err33 = RPCA_IPPS_ADMM_ERR(M, theta, w, beta1, L, S, T);
+err33 = RPCA_IPPS_ADMM_ERR(M, theta, w, beta2, L, S, T);
 
 err34 = RPCA_PRS_ADMM_ERR(M, theta, w, beta1, L, S, T);
 
 figure;
 ind = 0:3000;
-plot(ind, log10(err34), '-.', 'color', '#77AC30', 'Linewidth', 1.25);
+plot(ind, log10(err34), '-.', 'color', '#77AC30', 'Linewidth', 2);
 hold on;
-plot(ind, log10(err32), '--', 'color', 'm', 'Linewidth', 1.25);
+plot(ind, log10(err32), '--', 'color', '#A066D3', 'Linewidth', 2);
 hold on;
-plot(ind, log10(err33), ':', 'color', '#EDB120', 'Linewidth', 1.75);
+plot(ind, log10(err33), '-.', 'color', '#f97306', 'Linewidth', 2);
 hold on;
-plot(ind, log10(err31), '-', 'color', '#0072BD', 'Linewidth', 1.25);
+plot(ind, log10(err31), '-', 'color', '#0072BD', 'Linewidth', 2);
 hold off;
 ylim([-3 0]);
+grid on;
 set(gca, 'FontName', 'Times New Roman', 'Fontsize', 11);
 h = legend('PSR-ADMM', 'pADMMz', 'IPPS-ADMM', 'ADMMn', 'Location', 'northeast');
 set(h, 'FontSize', 8);
@@ -142,7 +147,7 @@ xlabel('Iterations');
 ylabel('log_{10}(RelErr)');
 
 
-% noisy case, (0.05，20)
+% noisy case, (0.05, 20)
 spr = 0.05;
 rank = 20;
 
@@ -165,20 +170,21 @@ err41 = RPCA_ADMMn_ERR(M, theta, w, beta1, L, S, T);
 
 err42 = RPCA_pADMMz_ERR(M, theta, w, beta1, L, S, T);
 
-err43 = RPCA_IPPS_ADMM_ERR(M, theta, w, beta1, L, S, T);
+err43 = RPCA_IPPS_ADMM_ERR(M, theta, w, beta2, L, S, T);
 
 err44 = RPCA_PRS_ADMM_ERR(M, theta, w, beta1, L, S, T);
 
 figure;
 ind = 0:3000;
-plot(ind, log10(err44), '-.', 'color', '#77AC30', 'Linewidth', 1.25);
+plot(ind, log10(err44), '-.', 'color', '#77AC30', 'Linewidth', 2);
 hold on;
-plot(ind, log10(err42), '--', 'color', 'm', 'Linewidth', 1.25);
+plot(ind, log10(err42), '--', 'color', '#A066D3', 'Linewidth', 2);
 hold on;
-plot(ind, log10(err43), ':', 'color', '#EDB120', 'Linewidth', 1.75);
+plot(ind, log10(err43), '-.', 'color', '#f97306', 'Linewidth', 2);
 hold on;
-plot(ind, log10(err41), '-', 'color', '#0072BD', 'Linewidth', 1.25);
+plot(ind, log10(err41), '-', 'color', '#0072BD', 'Linewidth', 2);
 hold off;
+grid on; 
 set(gca, 'FontName', 'Times New Roman', 'Fontsize', 11);
 h = legend('PSR-ADMM', 'pADMMz', 'IPPS-ADMM', 'ADMMn', 'Location', 'northeast');
 set(h, 'FontSize', 8);
@@ -186,4 +192,3 @@ xlabel('Iterations');
 ylabel('log_{10}(RelErr)');
 
 save test2
-load test2
