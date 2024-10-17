b7e68ee9f2aec173942610fe258c77c3d448454b
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2024-10-17 18:53:10 +0800

    Update prox_Lfrac12.m

--- RPCA/prox_Lfrac12.m
+++ RPCA/prox_Lfrac12.m
@@ -1,20 +1,29 @@
 % L1/2 shrinkage operator
-% Input: a given matrix X, with the parameter lambda
-% Output: the L1/2-shrink matrix Y
-% Written by: Zekun Liu (16/12/2023)
+%
+% Input: 
+%         X: a given matrix X
+%    lambda: the shrinkage parameter 
+%
+% Output: 
+%         Y: the L1/2-shrink matrix
+%
+% Written by Zekun Liu, 16/12/2023
+%
 % Reference:
-% [1] Z. Xu, X. Chang, F. Xu and H. Zhang. L1/2 Regularization: A Thresholding Representation Theory and a Fast Solver. 
+% [1] Z. Xu, X. Chang, F. Xu and H. Zhang. 
+%     L1/2 Regularization: A Thresholding Representation Theory and a Fast Solver. 
 %     IEEE Transactions on Neural Networks and Learning Systems, 23 (2012), 1013–1027.
-% Latest Revision: 20/09/2024
+%
+% Latest Revision: 17/10/2024
 
 
 function Y = prox_Lfrac12(X, lambda)
 
-t = 2/3;
+t = 2 / 3;
 m = size(X, 1);
 n = size(X, 2);
 Y = zeros(m, n);
-T = find(X > 54 ^ (1 / 3) / 4 * lambda^t);
-Y(T) = t * X(T) .* (1 + cos(t * pi - t * acos((lambda / 8) ./ ((abs(X(T)) / 3) .^ (1 / t)))));
+T = find(X > 54^(1 / 3) / 4 * lambda^t);
+Y(T) = t * X(T) .* (1 + cos(t * pi - t * acos((lambda / 8) ./ ((abs(X(T)) / 3).^(1 / t)))));
 
 end
