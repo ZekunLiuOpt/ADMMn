7dee30421f7558de784c6fb595ed4378f1c1002d
ZekunLiuOpt <79694300+ZekunLiuOpt@users.noreply.github.com>
2025-07-18 14:20:32 +0800

    Update NMC_test.m

--- NMC/NMC_test.m
+++ NMC/NMC_test.m
@@ -2,7 +2,7 @@
 %
 % Written by Zekun Liu, 06/02/2024
 %
-% Latest Revision: 17/10/2024
+% Latest Revision: 18/07/2025
 
 
 clear
@@ -10,37 +10,36 @@ clc
 
 m = 500;
 n = 500;
-RANK = [2; 10; 20];
-SR = [0.7; 0.5; 0.3];
+RANK = 0.01 * m : 0.01 * m : 0.1 * m;
+SR = [0.9, 0.7, 0.5, 0.3, 0.1];
 CNT = 20;
 
 w = 1;
 beta = 1;
 gamma = 1.618;
 
-Timesaver1 = zeros(3, 3, CNT);
-Timesaver2 = zeros(3, 3, CNT);
-Timesaver3 = zeros(3, 3, CNT);
+Timesaver1 = zeros(length(RANK), length(SR), CNT);
+Timesaver2 = zeros(length(RANK), length(SR), CNT);
+Timesaver3 = zeros(length(RANK), length(SR), CNT);
 
-Errsaver1 = zeros(3, 3, CNT);
-Errsaver2 = zeros(3, 3, CNT);
-Errsaver3 = zeros(3, 3, CNT);
+Errsaver1 = zeros(length(RANK), length(SR), CNT);
+Errsaver2 = zeros(length(RANK), length(SR), CNT);
+Errsaver3 = zeros(length(RANK), length(SR), CNT);
 
-Chgsaver1 = zeros(3, 3, CNT);
-Chgsaver2 = zeros(3, 3, CNT);
-Chgsaver3 = zeros(3, 3, CNT);
+Chgsaver1 = zeros(length(RANK), length(SR), CNT);
+Chgsaver2 = zeros(length(RANK), length(SR), CNT);
+Chgsaver3 = zeros(length(RANK), length(SR), CNT);
 
-Itersaver1 = zeros(3, 3, CNT);
-Itersaver2 = zeros(3, 3, CNT);
-Itersaver3 = zeros(3, 3, CNT);
+Itersaver1 = zeros(length(RANK), length(SR), CNT);
+Itersaver2 = zeros(length(RANK), length(SR), CNT);
+Itersaver3 = zeros(length(RANK), length(SR), CNT);
 
-for i = 1:3
-    for j = 1:3
+for i = 1:length(RANK)
+    r = RANK(i);
+    for j = 1:length(SR)
+        d = ceil(m * n * SR(j));
         for k = 1:CNT
         
-            r = RANK(i);
-            d = ceil(m * n * SR(j));
-
             X = rknonnegmatrix(m, n, r);  
             P = location(m, n, d);   
             M = P .* X; 
@@ -93,4 +92,128 @@ AveIter1 = mean(Itersaver1, 3);
 AveIter2 = mean(Itersaver2, 3);
 AveIter3 = mean(Itersaver3, 3);
 
-save test3
+save NMC500Time
+
+figure;
+plot(RANK, log10(AveErr1(:, 1)), '-', 'color', '#0072BD', 'Linewidth', 2);
+hold on;
+plot(RANK, log10(AveErr1(:, 2)), '-*', 'color', '#f97306', 'Linewidth', 2);
+hold on;
+plot(RANK, log10(AveErr1(:, 3)), '-x', 'color', '#A066D3', 'Linewidth', 2);
+hold on;
+plot(RANK, log10(AveErr1(:, 4)), '-o', 'color', '#77AC30', 'Linewidth', 2);
+hold on;
+plot(RANK, log10(AveErr1(:, 5)), '-+', 'color', '[0.93 0.69 0.13]', 'Linewidth', 2);
+hold off;
+grid on;
+xlim([min(RANK) max(RANK)])
+ylim([-7 0]);
+set(gca, 'FontName', 'Arial', 'Fontsize', 12);
+legend('sr 90%', 'sr 70%', 'sr 50%', 'sr 30%', 'sr 10%', 'Location', 'northwest');
+fontsize(12, "points")
+xlabel('rank');
+ylabel('log_{10}(RelErr)');
+
+figure;
+plot(RANK, log10(AveErr2(:, 1)), '-', 'color', '#0072BD', 'Linewidth', 2);
+hold on;
+plot(RANK, log10(AveErr2(:, 2)), '-*', 'color', '#f97306', 'Linewidth', 2);
+hold on;
+plot(RANK, log10(AveErr2(:, 3)), '-x', 'color', '#A066D3', 'Linewidth', 2);
+hold on;
+plot(RANK, log10(AveErr2(:, 4)), '-o', 'color', '#77AC30', 'Linewidth', 2);
+hold on;
+plot(RANK, log10(AveErr2(:, 5)), '-+', 'color', '[0.93 0.69 0.13]', 'Linewidth', 2);
+hold off;
+grid on;
+xlim([min(RANK) max(RANK)])
+ylim([-7 0]);
+set(gca, 'FontName', 'Arial', 'Fontsize', 12);
+legend('sr 90%', 'sr 70%', 'sr 50%', 'sr 30%', 'sr 10%', 'Location', 'northwest');
+fontsize(12, "points")
+xlabel('rank');
+ylabel('log_{10}(RelErr)');
+
+figure;
+plot(RANK, log10(AveErr3(:, 1)), '-', 'color', '#0072BD', 'Linewidth', 2);
+hold on;
+plot(RANK, log10(AveErr3(:, 2)), '-*', 'color', '#f97306', 'Linewidth', 2);
+hold on;
+plot(RANK, log10(AveErr3(:, 3)), '-x', 'color', '#A066D3', 'Linewidth', 2);
+hold on;
+plot(RANK, log10(AveErr3(:, 4)), '-o', 'color', '#77AC30', 'Linewidth', 2);
+hold on;
+plot(RANK, log10(AveErr3(:, 5)), '-+', 'color', '[0.93 0.69 0.13]', 'Linewidth', 2);
+hold off;
+grid on;
+xlim([min(RANK) max(RANK)])
+ylim([-7 0]);
+set(gca, 'FontName', 'Arial', 'Fontsize', 12);
+legend('sr 90%', 'sr 70%', 'sr 50%', 'sr 30%', 'sr 10%', 'Location', 'northwest');
+fontsize(12, "points")
+xlabel('rank');
+ylabel('log_{10}(RelErr)');
+
+
+figure;
+plot(RANK, AveTime1(:, 1), '-', 'color', '#0072BD', 'Linewidth', 2);
+hold on;
+plot(RANK, AveTime1(:, 2), '-*', 'color', '#f97306', 'Linewidth', 2);
+hold on;
+plot(RANK, AveTime1(:, 3), '-x', 'color', '#A066D3', 'Linewidth', 2);
+hold on;
+plot(RANK, AveTime1(:, 4), '-o', 'color', '#77AC30', 'Linewidth', 2);
+hold on;
+plot(RANK, AveTime1(:, 5), '-+', 'color', '[0.93 0.69 0.13]', 'Linewidth', 2);
+hold off;
+grid on;
+xlim([min(RANK) max(RANK)])
+ylim([min([min(min(AveTime1)), min(min(AveTime2)), min(min(AveTime3))]), ...
+      max([max(max(AveTime1)), max(max(AveTime2)), max(max(AveTime3))])]);
+set(gca, 'FontName', 'Arial', 'Fontsize', 12);
+legend('sr 90%', 'sr 70%', 'sr 50%', 'sr 30%', 'sr 10%', 'Location', 'northwest');
+fontsize(12, "points")
+xlabel('rank');
+ylabel('CPU Time (Seconds)');
+
+figure;
+plot(RANK, AveTime2(:, 1), '-', 'color', '#0072BD', 'Linewidth', 2);
+hold on;
+plot(RANK, AveTime2(:, 2), '-*', 'color', '#f97306', 'Linewidth', 2);
+hold on;
+plot(RANK, AveTime2(:, 3), '-x', 'color', '#A066D3', 'Linewidth', 2);
+hold on;
+plot(RANK, AveTime2(:, 4), '-o', 'color', '#77AC30', 'Linewidth', 2);
+hold on;
+plot(RANK, AveTime2(:, 5), '-+', 'color', '[0.93 0.69 0.13]', 'Linewidth', 2);
+hold off;
+grid on;
+xlim([min(RANK) max(RANK)])
+ylim([min([min(min(AveTime1)), min(min(AveTime2)), min(min(AveTime3))]), ...
+      max([max(max(AveTime1)), max(max(AveTime2)), max(max(AveTime3))])]);
+set(gca, 'FontName', 'Arial', 'Fontsize', 12);
+legend('sr 90%', 'sr 70%', 'sr 50%', 'sr 30%', 'sr 10%', 'Location', 'northwest');
+xlabel('rank');
+ylabel('CPU Time (Seconds)');
+fontsize(12, "points")
+
+figure;
+plot(RANK, AveTime3(:, 1), '-', 'color', '#0072BD', 'Linewidth', 2);
+hold on;
+plot(RANK, AveTime3(:, 2), '-*', 'color', '#f97306', 'Linewidth', 2);
+hold on;
+plot(RANK, AveTime3(:, 3), '-x', 'color', '#A066D3', 'Linewidth', 2);
+hold on;
+plot(RANK, AveTime3(:, 4), '-o', 'color', '#77AC30', 'Linewidth', 2);
+hold on;
+plot(RANK, AveTime3(:, 5), '-+', 'color', '[0.93 0.69 0.13]', 'Linewidth', 2);
+hold off;
+grid on;
+xlim([min(RANK) max(RANK)])
+ylim([min([min(min(AveTime1)), min(min(AveTime2)), min(min(AveTime3))]), ...
+      max([max(max(AveTime1)), max(max(AveTime2)), max(max(AveTime3))])]);
+set(gca, 'FontName', 'Arial', 'Fontsize', 12);
+legend('sr 90%', 'sr 70%', 'sr 50%', 'sr 30%', 'sr 10%', 'Location', 'northwest');
+fontsize(12, "points")
+xlabel('rank');
+ylabel('CPU Time (Seconds)');
